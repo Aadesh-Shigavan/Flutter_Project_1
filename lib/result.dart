a9e21d916cb0e57670ff57ddb2192036c677de94
Aadesh <aadesh.shigavan01@gmail.com>
2020-06-16 12:12:37 +0530

    Quiz completed

--- lib/result.dart
+++ lib/result.dart
@@ -2,18 +2,23 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultscore;
+  final Function resetHandler;
 
-  Result(this.resultscore);
+  Result(this.resultscore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
     if (resultscore <= 8) {
+      print(resultscore);
       resultText = 'You are Awesome';
     } else if (resultscore <= 12) {
+      print(resultscore);
       resultText = 'Preety Likeable';
     } else if (resultscore <= 16) {
+      print(resultscore);
       resultText = 'You are strange....!';
     } else {
+      print(resultscore);
       resultText = 'You are Bad!';
     }
 
@@ -23,10 +28,20 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text(
+              'Restart Quiz!'),
+              textColor: Colors.blue,
+            onPressed: resetHandler,
+          ),
+        ],
       ),
     );
   }
